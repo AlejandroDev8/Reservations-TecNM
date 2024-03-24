0759d68492dceda830ffbfcc0d4c8332af381710
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-03-23 20:01:22 -0600

    Update forgot password page with Spanish translation

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,36 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('¿Olvidaste tu constraseña? Coloca tu correo electrónico de registro y enviaremos un enlace para que
+        puedas crear una nueva') }}
     </div>
 
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('password.email') }}">
+    <form method="POST" action="{{ route('password.email') }}" novalidate>
         @csrf
+
+        <x-input-error :messages="str_replace('email', 'correo electrónico', $errors->get('email'))" class="mt-2" />
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <x-input-label for="email" :value="__('Correo electrónico')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+                autofocus />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+        <div class="flex justify-between my-5">
+            <x-link href="{{ route('login') }}">
+                Iniciar Sesión
+            </x-link>
+
+            <x-link href="{{ route('register') }}">
+                Crear Cuenta
+            </x-link>
+
         </div>
+        <x-primary-button class="w-full justify-center">
+            {{ __('Enviar enlace para restablecer contraseña') }}
+        </x-primary-button>
     </form>
 </x-guest-layout>
