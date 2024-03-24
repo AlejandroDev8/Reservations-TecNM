6c483ffa3236f3c96ab6fd7b363ee2f7b5d906c6
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-03-23 19:50:17 -0600

    Translate error messages and update login page layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,19 +5,19 @@
     <form method="POST" action="{{ route('login') }}" novalidate>
         @csrf
 
-        <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        <x-input-error :messages="str_replace('email', 'correo electrónico', $errors->get('email'))" class="mt-2" />
+        <x-input-error :messages="str_replace('password', 'contraseña', $errors->get('password'))" class="mt-2" />
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="__('Correo electrónico')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
                 autofocus autocomplete="username" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Contraseña')" />
 
             <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
                 autocomplete="current-password" />
@@ -28,21 +28,23 @@
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox"
                     class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                <span class="ms-2 text-sm text-gray-600">{{ __('Recordarme') }}</span>
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                href="{{ route('password.request') }}">
-                {{ __('Forgot your password?') }}
-            </a>
-            @endif
+        <div class="flex justify-between my-5">
 
-            <x-primary-button class="ms-3">
-                {{ __('Iniciar Sesión') }}
-            </x-primary-button>
+            <x-link href="{{ route('register') }}">
+                Crear Cuenta
+            </x-link>
+
+            <x-link href="{{ route('password.request') }}">
+                ¿Olvidaste tu contraseña?
+            </x-link>
+
         </div>
+        <x-primary-button class="w-full justify-center">
+            {{ __('Iniciar Sesión') }}
+        </x-primary-button>
     </form>
 </x-guest-layout>
