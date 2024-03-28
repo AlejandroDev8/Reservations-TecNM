6063d69213d4b04eebadbc2f360e2a1838faa0f0
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-03-27 18:52:32 -0600

    Update navigation links and name to route dashboard

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [ReservationController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [ReservationController::class, 'index'])->middleware(['auth', 'verified'])->name('reservation.index');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
