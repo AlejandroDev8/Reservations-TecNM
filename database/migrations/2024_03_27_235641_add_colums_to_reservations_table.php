e443ca488536e788c746813ef00472cde9cd70d6
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-03-27 18:01:57 -0600

    Add colums to reservations table

---
+++ database/migrations/2024_03_27_235641_add_colums_to_reservations_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('reservations', function (Blueprint $table) {
+            $table->string('email');
+            $table->foreignId('room_id')->constrained()->onDelete('cascade');
+            $table->date('date_in');
+            $table->date('date_out');
+            $table->foreignId('accommodation_id')->constrained()->onDelete('cascade');
+            $table->text('reasons');
+            $table->foreignId('state_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->text('answer')->nullable();
+
+            // create a composite unique key
+
+            $table->unique(['room_id', 'date_in', 'date_out']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('reservations', function (Blueprint $table) {
+            $table->dropColumn('email');
+            $table->dropForeign(['room_id']);
+            $table->dropColumn('room_id');
+            $table->dropColumn('date_in');
+            $table->dropColumn('date_out');
+            $table->dropForeign(['accommodation_id']);
+            $table->dropColumn('accommodation_id');
+            $table->dropColumn('reasons');
+            $table->dropForeign(['state_id']);
+            $table->dropColumn('state_id');
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+            $table->dropColumn('answer');
+            $table->dropUnique(['room_id', 'date_in', 'date_out']);
+        });
+    }
+};
